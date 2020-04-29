075f713e1ea9a6a0a2d7152c1b66e494dbc4987b
Maureen <1437867946@qq.com>
2020-04-29 15:54:57 +0800

    I/O流：Apache commons-io的使用

---
+++ src/com/maureen/commonsio/Commons_ioTest.java
@@ -0,0 +1,20 @@
+package com.maureen.commonsio;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.EmptyFileFilter;
+
+import java.io.File;
+import java.util.Collection;
+
+public class Commons_ioTest {
+    public static void main(String[] args) {
+        //返回文件大小
+        long length = FileUtils.sizeOf(new File("baidu.html"));
+        System.out.println(length);
+
+        Collection<File> files = FileUtils.listFiles(new File("c:"), EmptyFileFilter.NOT_EMPTY, null);
+        for(File file:files) {
+            System.out.println(file.getAbsolutePath());
+        }
+    }
+}
