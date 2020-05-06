a0ca3d92f501c955987941ed02b3155cb0d2ea77
Maureen <1437867946@qq.com>
2020-05-06 12:44:59 +0800

    多线程：线程池-SingleThreadExecutor

---
+++ src/com/maureen/threadpool/SingleThreadExecutorDemo.java
@@ -0,0 +1,15 @@
+package com.maureen.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleThreadExecutorDemo {
+    public static void main(String[] args) {
+        //线程池中只有一个线程
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for(int i = 0; i < 20; i++) {
+            executorService.execute(new Task());
+        }
+        executorService.shutdown();
+    }
+}
