23d99f884068a9d8bba2869141ad586ce82422de
Maureen <1437867946@qq.com>
2020-05-06 12:40:18 +0800

    多线程：线程池-FixedThreadPool

---
+++ src/com/maureen/threadpool/FixedThreadPoolDemo.java
@@ -0,0 +1,14 @@
+package com.maureen.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPoolDemo {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);//5表示最多可以容纳的线程数
+        for(int i = 0; i < 20; i++) {
+            executorService.execute(new Task());
+        }
+        executorService.shutdown();
+    }
+}
