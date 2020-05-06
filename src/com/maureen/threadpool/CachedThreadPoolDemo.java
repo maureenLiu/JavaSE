e67eaaed4753e69f88829464385700ba95956acb
Maureen <1437867946@qq.com>
2020-05-06 12:44:38 +0800

    多线程：线程池的相关demo添加注释

--- src/com/maureen/threadpool/CachedThreadPoolDemo.java
+++ src/com/maureen/threadpool/CachedThreadPoolDemo.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 
 public class CachedThreadPoolDemo {
     public static void main(String[] args) {
-        //创建线程池
+        //创建线程池，线程数无限制，最大可以是Integer.MAX_VALUE
         ExecutorService executorService = Executors.newCachedThreadPool();
         //提交任务
         for(int i = 0; i < 20; i++) {
--- src/com/maureen/threadpool/FixedThreadPoolDemo.java
+++ src/com/maureen/threadpool/FixedThreadPoolDemo.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Executors;
 
 public class FixedThreadPoolDemo {
     public static void main(String[] args) {
+        //自定义线程池中线程的个数
         ExecutorService executorService = Executors.newFixedThreadPool(5);//5表示最多可以容纳的线程数
         for(int i = 0; i < 20; i++) {
             executorService.execute(new Task());
