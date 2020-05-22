73252244ee1c0bad0bee6e391e6b85fdaa34ad8e
Maureen <1437867946@qq.com>
2020-05-22 15:20:36 +0800

    注解--为了减少使用自定义注解时每次都要填写参数的麻烦，所以可以在定义时规定好默认值，那么使用自定义注解的时候就不用再写参数了

--- src/com/maureen/annotation/metaannotation/MetaAnnotation.java
+++ src/com/maureen/annotation/metaannotation/MetaAnnotation.java
@@ -2,7 +2,8 @@ package com.maureen.annotation.metaannotation;
 
 import java.lang.annotation.*;
 
-@MyAnnotation(name="hehe",age=12, id = 3, likes={"book","tv"})
+//@MyAnnotation(name="hehe",age=12, id = 3, likes={"book","tv"})
+@MyAnnotation
 public class MetaAnnotation {
     public void test() {}
 }
@@ -17,8 +18,9 @@ public class MetaAnnotation {
 @Inherited
 @interface MyAnnotation{
     //定义的方式看起来像是方法，但实际上是使用注解的时候填写的参数名称，默认的名称是value
-    String name();
-    int age();
-    int id();
-    String[] likes();
+    //自定义注解中填写的所有方法都需要在使用注解的时候，添加值，很麻烦，因此包含默认值
+    String name() default "zhangsan";
+    int age() default 12;
+    int id() default 1;
+    String[] likes() default {"book","tv"};
 }
